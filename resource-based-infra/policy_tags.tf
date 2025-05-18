be2f816e20a0ee6427862ddff93a30e7f35aaf5a
Piotr Wozniak <piotrwozniak28@gmail.com>
2025-05-18 14:40:32 +0200

    WIP-0 work in progress

--- resource-based-infra/policy_tags.tf
+++ resource-based-infra/policy_tags.tf
@@ -3,13 +3,13 @@ resource "google_data_catalog_taxonomy" "this" {
   region                 = var.region
   display_name           = "tax-tpcds-${random_string.this.result}"
   description            = "A collection of policy tags"
-  activated_policy_types = []
+  activated_policy_types = ["FINE_GRAINED_ACCESS_CONTROL"]
 }
 
 resource "google_data_catalog_policy_tag" "this" {
   taxonomy     = google_data_catalog_taxonomy.this.id
-  display_name = "High"
-  description  = "A policy tag category used for high security access"
+  display_name = "PII_Data"
+  description  = "Policy tag for Personally Identifiable Information (PII) that requires controlled access"
 }
 
 # resource "google_data_catalog_policy_tag" "child_policy" {
@@ -28,12 +28,20 @@ resource "google_data_catalog_policy_tag" "this" {
 #   depends_on = [google_data_catalog_policy_tag.child_policy]
 # }
 
+# Grant fine-grained reader access to the user on the PII policy tag
+# resource "google_data_catalog_policy_tag_iam_member" "pii_reader" {
+#   policy_tag = google_data_catalog_policy_tag.this.id
+#   role       = "roles/datacatalog.categoryFineGrainedReader"
+#   member     = "user:gftdummyuser100@customcloudsolutions.pl"
+# }
+
 output "google_data_catalog_taxonomy" {
   value = google_data_catalog_taxonomy.this
 }
 
-output "google_data_catalog_policy_tag" {
-  value = google_data_catalog_policy_tag.this
+output "google_data_catalog_policy_tag_pii" {
+  value       = google_data_catalog_policy_tag.this
+  description = "Policy tag for PII data that requires controlled access"
 }
 # output "google_data_catalog_policy_tag_child_policy" {
 #   value = google_data_catalog_policy_tag.child_policy
